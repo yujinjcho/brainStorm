88d2a10a6ffd89b1cad6a0e6641a23967654eec8
Yujin Cho <yujinjcho@gmail.com>
2016-06-13 19:36:45 -0700

    changed chain in user-search

--- app/static/js/user-search.js
+++ app/static/js/user-search.js
@@ -7,14 +7,12 @@ $('#autocomplete').autocomplete({
     onSelect: function (suggestion) {
         //alert('You selected: ' + suggestion.value + ' ' + suggestion.id.toString();)
         var active_session = document.getElementById('new-idea').name;
-        $.when(
-	  	    app.permissionList.create({"granted_id":suggestion.id, 'session':active_session},{wait:true})
-	  	).then(function(){
-            app.userList.fetch({wait:true, reset: true})
-	  	}).then(function(){
-            document.getElementById('autocomplete').value='';
-        })
-
+        
+  	    app.permissionList.create({"granted_id":suggestion.id, 'session':active_session},{wait:true, success: function(){
+            app.userList.fetch({wait:true, reset: true, success: function(){
+                document.getElementById('autocomplete').value='';
+            }})
+        }})
 	  	//app.ratedIdeaList.fetch({wait:true, reset:true});
 
         //app.permissionList.create(
